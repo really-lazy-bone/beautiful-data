24d412b4ce755ef14cf792895ea1358c739a7716
Jonathan Kroening <jkroening@users.noreply.github.com>
2014-01-27 11:25:14 -0800

    Update downloadflickr.py

--- downloadflickr.py
+++ downloadflickr.py
@@ -18,14 +18,29 @@ locs = []
 urllist = [] #store a list of what was downloaded
 
 # downloading images
-for k in f[1:5]:
-    count = 1
-    if k.tags is not None:
-        for t in k.tags:
+count = 1
+for photo in f[1:100]:
+    tags = []
+    if photo.tags is not None:
+        for t in photo.tags:
             tags.append([count, t.raw])
-            print t.raw
-    print k.getLocation()
-    locs.append([count, k.getLocation()])
+    locs = []
+    if 'attrib' in photo:
+        if photo.attrib['latitude'] != '0':
+            loc = {}
+            loc['plugin'] = "flickr"
+            photo_link = unicode('http://www.flickr.com/photos/%s/%s' % (photo.attrib['owner'], photo.attrib['id']), 'utf-8')
+            title = photo.attrib['title']
+            #If the title is a string, make it unicode
+            if isinstance(title,str):
+                title = title.decode('utf-8')
+            loc['context'] = u'Photo from flickr  \n Title : %s \n ' % (title)
+            loc['date'] = datetime.datetime.strptime(photo.attrib['datetaken'], "%Y-%m-%d %H:%M:%S")
+            loc['lat'] = photo.attrib['latitude']
+            loc['lon'] = photo.attrib['longitude']
+            loc['shortName'] = "Unavailable"
+            loc['infowindow'] = self.constructContextInfoWindow(photo_link, loc['date'])
+            locs.append([count, loc])
     count += 1
     # p = k._load_properties()
     # print p.tags
